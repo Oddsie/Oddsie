018810f4345a10d4b181ee977f98d35300a50bec
Oddsie Dev <113137489+Oddsie@users.noreply.github.com>
2022-09-13 18:17:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ echo "Where is Oddsie located? (full directory)"
 read location
 cd ~
 echo python3 oddsie.py $1 $2 > oddsie
-echo alias oddsie="/home/$location/oddsie">>.bashrc
+echo alias oddsie="$location/oddsie">>.bashrc
