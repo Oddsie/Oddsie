a2e274ac0376a7d019c95f78990607ab8495e80f
Oddsie Dev <113137489+Oddsie@users.noreply.github.com>
2022-09-15 17:48:57 +0300

    added return equivelant

--- oddsie.py
+++ oddsie.py
@@ -80,6 +80,9 @@ def trans(name):
 			for i in range(l_t):l_i=l_i+"\t"
 			o_l.append(l_i+"except:")
 			print("KEYWORD FOUND: ERROR")
+		elif tokens[0]=="give":
+			for i in range(l_t):l_i=l_i+"\t"
+			o_l.append(l_i+"return "+tokens[1])
 		else:print("WARNING: No keywords detected at line "+str(l_n))
 		if line.endswith(":"):l_t+=1
 		l_n+=1
