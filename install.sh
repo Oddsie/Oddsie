1eefd5631aa35d0df34e86534fd59a18811ba52f
Oddsie Dev <113137489+Oddsie@users.noreply.github.com>
2022-10-23 19:16:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,4 @@
 cp oddsie.py ~
 cd ~
 chmod +x oddsie.py
-echo 'function oddsie () {' >> .bashrc
-echo './oddsie.py $1 $2' >> .bashrc
-echo '}' >> .bashrc
-echo 'export -f oddsie' >> .bashrc
+echo 'alias oddsie="./oddsie.py \$1 \$2"' >> .bashrc
